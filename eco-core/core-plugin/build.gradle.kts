ef459a3e7292ff9f4c99507fc4db4b27f2b5acc1
starfish2994 <LUJIAHAOFISH@outlook.com>
2025-11-01 18:25:28 +0800

    Update build.gradle.kts

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -18,26 +18,3 @@ tasks {
         dependsOn("publishToMavenLocal")
     }
 }
-
-publishing {
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            version = project.version.toString()
-            artifactId = rootProject.name
-
-            artifact(rootProject.tasks.shadowJar.get().archiveFile)
-        }
-    }
-
-    repositories {
-        maven {
-            name = "auxilor"
-            url = uri("https://repo.auxilor.io/repository/maven-releases/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
-    }
-}
